1272710e7f2637bd08e4b6977f90a3355e73e425
yazzidbustommy <yazzidbustommy28@gmail.com>
2021-06-01 14:50:56 +0700

    finish sistem login

--- body.php
+++ body.php
@@ -61,17 +61,11 @@
                 <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
                     <ul class="navbar-nav ml-auto">
                         <li class="nav-item dropdown no-arrow">
-                            <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <span class="mr-2 d-none d-lg-inline text-gray-600 small">Nama Guru</span>
-                                <i class="fas fa-user"></i>
+                            <a href="../logout.php">
+                                <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                                Logout
                             </a>
-                            <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
-                                <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
-                                    <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-                                    Logout
-                                </a>
-                            </div>
+
                         </li>
                     </ul>
                 </nav>
--- head.php
+++ head.php
@@ -1,3 +1,10 @@
+<?php
+session_start();
+if (!isset($_SESSION['log'])) {
+    header('Location:../index.php');
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 
