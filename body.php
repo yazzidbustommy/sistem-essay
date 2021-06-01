96d3a1972298145908aab1a11444c81cc9adefac
yazzidbustommy <yazzidbustommy28@gmail.com>
2021-06-01 15:04:06 +0700

    alert logout

--- body.php
+++ body.php
@@ -60,13 +60,10 @@
                 <!-- Topbar -->
                 <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
                     <ul class="navbar-nav ml-auto">
-                        <li class="nav-item dropdown no-arrow">
-                            <a href="../logout.php">
-                                <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-                                Logout
-                            </a>
-
-                        </li>
+                        <a href="../logout.php" onclick="return confirm('Yakin Ingin Logout?')">
+                            <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                            Logout
+                        </a>
                     </ul>
                 </nav>
                 <!-- End Topbar -->
