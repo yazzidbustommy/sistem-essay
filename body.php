7759a1e3f0afbf6ced6415f2fe599664c7876be2
yazzidbustommy <yazzidbustommy28@gmail.com>
2021-06-03 10:54:48 +0700

    tambah button navbar

--- body.php
+++ body.php
@@ -59,6 +59,9 @@
             <div id="content">
                 <!-- Topbar -->
                 <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+                    <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
+                        <i class="fa fa-bars"></i>
+                    </button>
                     <ul class="navbar-nav ml-auto">
                         <a href="../logout.php" onclick="return confirm('Yakin Ingin Logout?')">
                             <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
