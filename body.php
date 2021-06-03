44fd36c186a900ac78d172b17a0bb4611e328dc5
yazzidbustommy <yazzidbustommy28@gmail.com>
2021-06-03 11:50:35 +0700

    alert logout 2

--- body.php
+++ body.php
@@ -63,10 +63,19 @@
                         <i class="fa fa-bars"></i>
                     </button>
                     <ul class="navbar-nav ml-auto">
-                        <a href="../logout.php" onclick="return confirm('Yakin Ingin Logout?')">
-                            <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-                            Logout
-                        </a>
+                        <li class="nav-item dropdown no-arrow">
+                            <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <span class="mr-2 d-none d-lg-inline text-gray-600 small"></span>
+                                <i class="fas fa-user"></i>
+                            </a>
+                            <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
+
+                                <a class="dropdown-item" href="../logout.php" onclick="return confirm('Yakin Ingin Logout?')">
+                                    <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                                    Logout
+                                </a>
+                            </div>
+                        </li>
                     </ul>
                 </nav>
                 <!-- End Topbar -->
